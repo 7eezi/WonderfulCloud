0e6ce49e1bde307a2a8d5d2c90a13e9e372cabad
7eezi <46866623+7eezi@users.noreply.github.com>
2019-09-10 17:45:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+const ms = require("ms")
+const fs = require("fs")
+const prefix = '$W';
+
+
+
+bot.on('ready', () =>{
+    console.log(`
+    
+    - This Bot is Online.
+    - Logged in **${bot.user.tag}**.
+    - All Codes Working.
+
+    `)
+})
+
+
+
+
+
+
+
+
+
+
+bot.login(process.env.TOKEN);
